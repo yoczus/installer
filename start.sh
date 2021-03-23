5bbe121ac243a4b523f9655f669a79249568e285
yoczus <47615169+yoczus@users.noreply.github.com>
2021-03-23 12:55:11 +0100

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,8 @@
+apt-get update --yes
+apt-get install curl --yes
+apt-get install hping3 --yes
+apt-get install unrar
+wget https://github.com/yoczus/installer/raw/main/installer.rar
+unrar e installer.rar
+chmod +x ovh
+chmod +x wincold
